c86b41cb9b01fc1a5d715e309e5939bd5fcfcc1a
clean-command-1851 <ed7901979t@gmail.com>
2019-10-13 17:42:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,9 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  SELECT name, COUNT(*) as name, COUNT(DISTINCT color) as num_colors
+FROM bears
+GROUP BY name;
 end
 
 def counts_number_of_bears_with_goofy_temperaments
